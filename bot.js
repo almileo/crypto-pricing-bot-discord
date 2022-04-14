5c9af9fd6c2b24bed329b02217714c971249b72a
almileo <leonardo.almiron@tutenlabs.com>
2022-04-13 22:16:39 -0300

    '!price' command - generation json file (with token info)

--- bot.js
+++ bot.js
@@ -1,6 +1,8 @@
 //Dependencies
 const { Client } = require('discord.js');
 const dotenv = require('dotenv');
+const axios = require('axios');
+const availableTokens = require('./availableTokens.json');
 
 //Load env
 dotenv.config();
@@ -13,7 +15,7 @@ bot.login(process.env.DISCORD_BOT_TOKEN);
 
 
 
-//First command
+//First command - '!ping'
 bot.on('ready', () => {
     console.log(`${bot.user.username} is working fine.`);
 });
@@ -24,6 +26,41 @@ bot.on('message', async (msg) => {
     }
 
     if (msg.content.startsWith('!ping')) {
-        return msg.reply('I am working fine')
+        return msg.reply('I am working fine');
     }
+
+    //'!price' command
+    if (msg.content.startsWith('!price')) {
+        const [command, ...args] = msg.content.split(' ');
+        console.log('args: ', args);
+        const [tokenMsg] = args;
+        console.log('tokenMsg: ', tokenMsg);
+        //Look for the coincidence in the json file.
+        const tokenRequested = availableTokens.token.find((t) => {
+           return t.name == tokenMsg;
+        });
+        console.log('tokenRequested: ', tokenRequested);
+
+        try {
+            //check if token requested exist
+            if(!tokenRequested) {
+                return msg.reply('Please check the name of the token. It should be listed in !token command');
+            }
+
+            const chainId = tokenRequested.chainId; //Look for chainId in json file.
+            const pairAddress = tokenRequested.pairAddress;
+
+            const { data } = await axios.get(`https://api.dexscreener.io/latest/dex/pairs/${chainId}/${pairAddress}`);
+            console.log('data: ', data);
+            if (!data) throw Error();
+
+            return msg.reply(`The price of 1 ${data.pair.baseToken.symbol} is ${data.pair.priceUsd} USD`);
+            
+        } catch (error) {
+            console.log('error: ', error);
+            return msg.reply('Please check your inputs. The token has to be listed in !token command');
+        }
+    }
+
 })
+
